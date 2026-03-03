74ce2b8669d13e25204d08106b755ae85890ee6e
vbetsch <vic.betsch@gmail.com>
2026-03-03 18:58:55 +0100

    feat: update prettier conf

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,8 @@
+// import baseConfig from '@vbetsch/config/prettier/base';
 import cliConfig from '@vbetsch/config/prettier/cli';
 
 export default {
+  // ...baseConfig,
   ...cliConfig,
-  // bracketSpacing: false,
+  // singleQuote: false,
 };
