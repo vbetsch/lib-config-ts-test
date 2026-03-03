4afbcc8d49403d8e845178bc050325aaadf3ee70
vbetsch <vic.betsch@gmail.com>
2026-03-03 18:54:49 +0100

    test: update prettier

--- tests/prettier.test.ts
+++ tests/prettier.test.ts
@@ -1,7 +1,10 @@
 console.log('semi')
 console.log("double quote");
 console.log("printWidth -----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+function test() {
   console.log('indent - 2 spaces');
     console.log('indent - 4 spaces');
   console.log('indent - 1 tab');
     console.log('indent - 2 tabs');
+}
