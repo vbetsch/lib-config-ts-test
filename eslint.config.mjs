cd81450f836d2344302d4439e0f6f60fefe7ea81
vbetsch <vic.betsch@gmail.com>
2026-03-04 20:03:22 +0100

    test: strict eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,23 @@
-import prettierConfig from '@vbetsch/lib-config-ts/eslint/prettier';
+import tseslint from 'typescript-eslint';
+import base from '@vbetsch/lib-config-ts/eslint/base';
+import strict from '@vbetsch/lib-config-ts/eslint/strict';
+import prettier from '@vbetsch/lib-config-ts/eslint/prettier';
 
-export default prettierConfig;
+export default tseslint.config(
+  ...base,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  strict,
+  ...prettier,
+  {
+    rules: {
+      'prettier/prettier': 'error',
+    }
+  }
+);
