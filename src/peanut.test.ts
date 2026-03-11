78bfa1cf1c11f62ea77e2b7466dabffab503cdc2
vbetsch <vic.betsch@gmail.com>
2026-03-11 18:19:53 +0100

    test: improve peanut

--- src/peanut.test.ts
+++ src/peanut.test.ts
@@ -3,11 +3,12 @@ import { Peanut } from './peanut';
 describe('Peanut', () => {
   it('should pass', () => {
     const peanut = new Peanut();
+    expect(typeof peanut).toBe('object');
     expect(peanut).toBeInstanceOf(Peanut);
-    expect(peanut.name).toBe('This is an Peanut');
+    expect(peanut.name).toBe('This is a Peanut');
   });
   it('should not pass', () => {
     const peanut = new Peanut();
-    expect(typeof peanut).toBe('Peanut');
+    expect(peanut.name).toBe('This is a peanut butter');
   });
 });
