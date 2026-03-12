b3809591bed06c388e703350bcf500c1acc793fb
vbetsch <vic.betsch@gmail.com>
2026-03-12 18:28:24 +0100

    test: index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-import eslintBase from '@vbetsch/lib-config-ts/eslint/base';
-import eslintStrict from '@vbetsch/lib-config-ts/eslint/strict';
-
-// eslint
-console.log(eslintBase);
-console.log(eslintStrict);
+console.log('Hello World');
