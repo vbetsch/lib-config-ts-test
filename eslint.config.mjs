2550dde91f003d90152756f9301659d98b6cd2c2
vbetsch <vic.betsch@gmail.com>
2026-04-03 22:36:18 +0200

    fix: rename imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
 import tseslint from 'typescript-eslint';
-import base from '@vbetsch/config-eslint/base';
-// import jsx from '@vbetsch/config-eslint/jsx';
-// import naming from '@vbetsch/config-eslint/naming';
-// import prettier from '@vbetsch/config-eslint/prettier';
-// import spellcheck from '@vbetsch/config-eslint/spellcheck';
-// import strict from '@vbetsch/config-eslint/strict';
+import baseVbetsch from '@vbetsch/config-eslint/base';
+import jsxVbetsch from '@vbetsch/config-eslint/jsx';
+import namingVbetsch from '@vbetsch/config-eslint/naming';
+import prettierVbetsch from '@vbetsch/config-eslint/prettier';
+import spellcheckVbetsch from '@vbetsch/config-eslint/spellcheck';
+import strictVbetsch from '@vbetsch/config-eslint/strict';
 
 export default tseslint.config(
-  ...base,
-  // strict,
-  // naming,
-  // jsx,
-  // spellcheck,
-  // ...prettier,
+  ...baseVbetsch,
+  // strictVbetsch,
+  // namingVbetsch,
+  // jsxVbetsch,
+  // spellcheckVbetsch,
+  // ...prettierVbetsch,
 
   // IMPORTANT: You have to add these lines only for the strict module !
   // {
